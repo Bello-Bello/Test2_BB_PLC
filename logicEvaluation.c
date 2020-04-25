0c30ee03601117a36a0b49ac59d475727422e42b
Bello-Bello <60788906+Bello-Bello@users.noreply.github.com>
2020-04-25 18:17:20 -0400

    Add files via upload

---
+++ logicEvaluation.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main(){
+    int a=8, b=4, c=2;
+    
+    printf("\n a>b>c evaluates to %d \n", a>b>c);
+    printf(" a>b && b>c evaluates to %d \n", a>b && b>c);
+    
+    return 0;
+}
