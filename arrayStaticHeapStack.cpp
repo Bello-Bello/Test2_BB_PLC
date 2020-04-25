446477efe211ac5909e0e7ac25b9d2460953df1d
Bello-Bello <60788906+Bello-Bello@users.noreply.github.com>
2020-04-25 18:39:41 -0400

    Add files via upload

---
+++ arrayStaticHeapStack.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void staticArray();
+void heapArray(int n);
+void stackArray(int n);
+
+int main(){
+    int start,stop;
+    start = clock();
+    
+    for(int i=0;i<20000;i++){
+        staticArray();
+    }
+    stop = clock();
+    cout<<"Time taken by static method: "<<(stop - start)<<" ms"<<endl;
+  
+
+    start = clock();
+   
+    for(int i=0;i<20000;i++){
+        stackArray(200000);
+    }
+    stop = clock();
+    cout<<"Time taken by onStack method: "<<(stop - start)<<" ms"<<endl;
+
+    start = clock();
+    for(int i=0;i<20000;i++){
+        heapArray(200000);
+    }
+    stop = clock();
+    cout<<"Time taken by onHeap method: "<<(stop - start)<<" ms"<<endl;
+    }
+
+    void staticArray(){
+    int array[200000];
+    }
+    
+    void heapArray(int n){
+    int *array = (int*)malloc(sizeof(int)*200000);
+    }
+    
+    void stackArray(int n){
+    int array[n];
+    }
+
